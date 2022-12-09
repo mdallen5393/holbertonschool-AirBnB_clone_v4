92b46623b2a63a723b2d3e8c83a8f7a553d4cc73
VirtualBlakeWillis <bewillis101@gmail.com>
2022-12-09 21:27:51 +0000

    task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -11,9 +11,10 @@ $(document).ready(function() {
     }
     $('div.amenities h4').text(listOfCheckedAmenities.join(', '));
 
-    getStatus('http://d1eb7851f2e7.7399d2e2.hbtn-cod.io:5001/api/v1/status/');
-    places('http://d1eb7851f2e7.7399d2e2.hbtn-cod.io:5001/api/v1/places_search/');
+
   });
+  getStatus('http://d1eb7851f2e7.7399d2e2.hbtn-cod.io:5001/api/v1/status/');
+  places('http://d1eb7851f2e7.7399d2e2.hbtn-cod.io:5001/api/v1/places_search/');
 })
 
 function getStatus (url) {
@@ -27,7 +28,47 @@ function getStatus (url) {
 }
 
 function places (url) {
-  $.post(
-
-  );
+  $.ajax({
+    url: url,
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({}),
+    success: function(data) {
+      for (const place of data) {
+        $.get('http://d1eb7851f2e7.7399d2e2.hbtn-cod.io:5001/api/v1/users/' + place.user_id, function (userData) {
+          let guestsPlural = '';
+          if (place.max_guest != 1) {
+            let guestsPlural ='s';
+          } 
+          let roomsPlural = ''
+          if (place.number_rooms != 1) {
+            let roomsPlural ='s';
+          }
+          let bathroomPlural = ''
+          if (place.number_bathrooms != 1) {
+            let bathroomPlural ='s';
+          } 
+          let html = `<article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest${guestsPlural}</div>
+                  <div class="number_rooms">${place.number_rooms } Bedroom${roomsPlural}</div>
+                  <div class="number_bathrooms">${place.number_bathrooms} Bathroom${bathroomPlural}</div>
+          </div>
+          <div class="user">
+                  <b>Owner:</b> ${userData.first_name} ${userData.last_name}
+                </div>
+                <div class="description">
+            ${place.description}
+                </div>
+            </article>`;
+        $('.places').append(html);
+        });
+      }
+    }
+  });
 }
