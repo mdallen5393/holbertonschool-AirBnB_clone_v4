11a9f079df85ea19a9cd30fec1f16e592900704c
mdallen5393 <allen_matthew@outlook.com>
2023-01-05 09:55:43 -0600

    ??

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ let nums;
 if (process.argv.length < 4) {
   console.log('0');
 } else {
-  nums = [...process.argv];
+  nums = process.argv;
   nums = nums.slice(2).map(num => parseInt(num));
   const max = Math.max(...nums);
   nums = nums.filter(num => num !== max);
